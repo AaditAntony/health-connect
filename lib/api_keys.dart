4072e0b4855c672633c6b8a773e33c37ed627773
Aadit Antony <aaditantony1112@gmail.com>
2026-04-06 13:49:04 +0530

    done

--- lib/api_keys.dart
+++ lib/api_keys.dart
@@ -1,4 +1,4 @@
 class ApiKeys {
   // TODO: Replace this with your actual Google Gemini API Key
-  static const String geminiApiKey = 'AIzaSyAdCR6VxJLG3kM9xQ3M-RoczSf5QEKKLqE';
+  static const String geminiApiKey = 'AIzaSyCGMBdNEUpcIj8dFueeOdEVmuKoVOy-Y7g';
 }
